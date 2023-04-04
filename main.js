d6c5b9ac567ed2713b77f56ba144473fe6d491c2
Keith__ <30805331+AtomicKeith@users.noreply.github.com>
2023-04-04 11:05:08 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,15 @@
+const player = {
+  name: "Dr.Elias Shaw",
+  title: "Researcher",
+  class: "C",
+  clearance: 2
+};
+
+const score = {
+  data: 0
+};
+
+const dclass = {
+  amount: 0,
+  cost: 15
+};
