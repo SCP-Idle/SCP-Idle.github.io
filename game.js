e9c054b79cd0e42c118edd00d1b1c7af29cd3dad
Keith__ <30805331+AtomicKeith@users.noreply.github.com>
2023-05-12 10:41:56 -0500

    Update game.js

--- game.js
+++ game.js
@@ -1,9 +1,10 @@
 $(document).ready(function() {
   var data = 0;
   var upgrade1Cost = 10;
+  var upgrade1Value = 1;
 
   function update() {
-    data += 1;
+    data += upgrade1Value;
     $("#data").text(data);
 
     if (data >= upgrade1Cost) {
@@ -13,9 +14,15 @@ $(document).ready(function() {
 
   setInterval(update, 1000);
 
+  $("#click").click(function() {
+    data += 1;
+    $("#data").text(data);
+  });
+
   $("#upgrade1").click(function() {
     data -= upgrade1Cost;
     upgrade1Cost *= 2;
+    upgrade1Value += 1;
     $("#data").text(data);
     $(this).text("Upgrade 1 (Cost: " + upgrade1Cost + ")");
   });
